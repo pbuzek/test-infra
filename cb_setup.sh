391a2fa35c057a0758715d014a30ba2368dc56d7
pbuzek <pavel.buzek@gmail.com>
2019-11-22 14:54:58 -0800

    Update cb_setup.sh

--- cb_setup.sh
+++ cb_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p src/knative.dev
-ls | grep -v src | xargs mv -t src/knative.dev
+ls | grep -v src | xargs mv -t src/knative.dev/test-infra
 ls src/knative.dev
 cd src/knative.dev/test-infra
