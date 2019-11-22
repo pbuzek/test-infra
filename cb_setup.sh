28f148c49c0a7b081f1c6d2357f198d6c27657f7
pbuzek <pavel.buzek@gmail.com>
2019-11-22 15:39:16 -0800

    Update cb_setup.sh

--- cb_setup.sh
+++ cb_setup.sh
@@ -3,5 +3,6 @@ mkdir -p src/knative.dev/test-infra
 ls | grep -v src | xargs mv -t src/knative.dev/test-infra
 ls src/knative.dev
 cd src/knative.dev/test-infra
+sudo apt-get install wget
 wget https://github.com/bazelbuild/bazel/releases/download/1.2.0/bazel_1.2.0-linux-x86_64.deb
 sudo dpkg -i bazel_1.2.0-linux-x86_64.deb
